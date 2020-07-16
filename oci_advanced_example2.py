9f462d26e7f4fafc85beb3459db650dd2c0b09af
crciuperca@gmail.com <crciuperca@gmail.com>
2020-07-16 11:03:51 +0300

    added missing args

--- oci_advanced_example2.py
+++ oci_advanced_example2.py
@@ -26,21 +26,23 @@ dataflow_appname = "some_app_name"
 
 
 with dag:
-    t1 = OCIDataFlowCreateApplication(task_id='Create_Dataflow_Application_{0}'.format(dataflow_appname),
-                                      compartment_id="ocid1.compartment.oc1..aaaaaaaa64t4n5eposuegupgdtggjah2lyp7zbqmbpssvwf55q6gnnsnwpyq",
+    t1 = OCIDataFlowCreateApplication(task_id='Create_Dataflow_Application_BAS',
+                                      compartment_ocid="ocid1.compartment.oc1..aaaaaaaa64t4n5eposuegupgdtggjah2lyp7zbqmbpssvwf55q6gnnsnwpyq",
                                       display_name="Data Flow Test App",
                                       driver_shape="VM.Standard2.1",
                                       executor_shape="VM.Standard2.1",
                                       num_executors=1,
                                       spark_version="2.4.4",
                                       file_uri="oci://bas_test_bucket@osvcstage/bas-sparktest.jar",
+                                      object_name="oci://bas_test_bucket@osvcstage/bas-sparktest.jar",
                                       language="SCALA",
                                       class_name="HelloWorld",
                                       )
     t2 = OCIDataFlowRun(task_id='Run_Dataflow_Application_{0}'.format(dataflow_appname),
                         compartment_ocid="ocid1.compartment.oc1..aaaaaaaa64t4n5eposuegupgdtggjah2lyp7zbqmbpssvwf55q6gnnsnwpyq",
-                        display_name=dataflow_appname,
-                        oci_conn_id=oci_conn_id,
-                        bucket_name=bucketname
+                        display_name="bas_test_app",
+                        oci_conn_id="oci_default",
+                        bucket_name="acs"
                         )
     t1 >> t2
+# Broken DAG: [/opt/airflow/dags/oci_advanced_example2.py] Argument ['bucket_name', 'oci_conn_id', 'compartment_ocid', 'object_name'] is required
