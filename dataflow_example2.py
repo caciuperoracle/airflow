690950f5b9c7e1365c39edb49442583df354bda8
crciuperca@gmail.com <crciuperca@gmail.com>
2020-07-17 11:37:04 +0300

    removed bucket creation

---
+++ dataflow_example2.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from airflow import DAG
+from operators.oci_object_storage import MakeBucket,  CopyFileToOCIObjectStorageOperator
+from operators.oci_data_flow import OCIDataFlowRun, OCIDataFlowCreateApplication
+
+default_args = {'owner': 'airflow',
+                'start_date': datetime(2020, 5, 26),
+                'email': ['your_email@somecompany.com'],
+                'email_on_failure': False,
+                'email_on_retry': False
+                }
+
+dag = DAG('dataflow_example2',
+          default_args=default_args,
+          schedule_interval='@hourly',
+          catchup=False
+          )
+
+oci_conn_id = "oci_default"
+bucketname = "bas-airflow-test2"
+compartment_ocid = "ocid1.compartment.oc1..aaaaaaaa64t4n5eposuegupgdtggjah2lyp7zbqmbpssvwf55q6gnnsnwpyq"
+dataflow_file = "dataflow_example.py"
+dataflow_appname = "bas_test"
+file_path = "D:/OracleWorkspace/oci-airflow/scripts/dags/"
+
+with dag:
+    t1 = OCIDataFlowCreateApplication(task_id='Create_Dataflow_Application_{0}'.format(dataflow_appname),
+                                      bucket_name=bucketname,
+                                      display_name=dataflow_appname,
+                                      compartment_ocid=compartment_ocid,
+                                      oci_conn_id=oci_conn_id,
+                                      object_name=dataflow_file,
+                                      language='PYTHON',
+                                      )
+    t2 = OCIDataFlowRun(task_id='Run_Dataflow_Application_{0}'.format(dataflow_appname),
+                        compartment_ocid=compartment_ocid,
+                        display_name=dataflow_appname,
+                        oci_conn_id=oci_conn_id,
+                        bucket_name=bucketname
+                        )
+    t1 >> t2
